9979f0b23d1c62b66870c38de98346b641d977ba
MdShafiqulSaymon <mdshafiqulsaymon@gmail.com>
2025-07-24 15:51:40 +0600

    Test File added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/My React App with Docker & CI\/CD/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders deployment message', () => {
+  render(<App />);
+  const messageElement = screen.getByText(/Successfully deployed with GitHub Actions!/i);
+  expect(messageElement).toBeInTheDocument();
 });
